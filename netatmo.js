240bc53ecdac46591e1e6836585428c2e743b0ed
ec2-user <bjornth.aws@gmail.com>
2025-01-02 19:39:57 +0000

    First commit

---
+++ netatmo.js
@@ -0,0 +1,11 @@
+const Netatmo = require(`Netatmo`)
+
+const credentials = {
+    client_id   =   `6776e56ddcec7aba070cf9cc`,
+    client_secret = `OHqpCIkcobzzAyHkhtpg9dCOhgVdEzknHYlYQohTbV7lQ`,
+    username = "bjornth@gmail.com",
+    password    = `yPI3xV9zS`,
+    scope       = `read_station`
+};
+
+
